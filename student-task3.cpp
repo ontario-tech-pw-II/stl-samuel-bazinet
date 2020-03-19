ad34c158c51d370306cbfdaca8eddde9c1090c68
samuel-bazinet <59840350+samuel-bazinet@users.noreply.github.com>
2020-03-19 14:02:41 -0400

    Add files via upload

---
+++ student-task3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+int main() {
+
+    ifstream fin("sample.txt");
+
+    string out1;
+    string out2;
+    int sout2;
+    map<string,int> c;
+    map<string,int>::iterator i;
+
+
+    while (!fin.eof()) {
+        fin >> out1;
+        fin >> out2;
+        sout2 = stoi(out2);
+        if (sout2 < 50) {
+            if (c.find(out1) == c.end()) {
+                c[out1] = 1;
+            } else {
+                c[out1]++;
+            }
+        } else {
+            if (c.find(out1) == c.end()) {
+                c[out1] = 0;
+            }
+        }
+    }
+
+    for (i = c.begin(); i != c.end(); i++) {
+        cout << i->first << ' ' << i->second << endl; 
+    }
+
+    return 0;
+}
