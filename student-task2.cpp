021ce7fa27d05a697386270f42ff8db9cb30a10b
samuel-bazinet <59840350+samuel-bazinet@users.noreply.github.com>
2020-03-19 13:54:45 -0400

    Add files via upload

---
+++ student-task2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+int main() {
+
+    ifstream fin("sample.txt");
+
+    string out1;
+    string out2;
+    int sout2;
+    set<string> c;
+
+    while (!fin.eof()) {
+        fin >> out1;
+        fin >> out2;
+        sout2 = stoi(out2);
+        if (sout2 < 50) {
+            c.insert(out1);
+        }
+    }
+
+    set<string>::iterator i;
+    for (i = c.begin(); i != c.end(); i++) {
+        cout << *i << endl;
+    }
+
+    return 0;
+}
